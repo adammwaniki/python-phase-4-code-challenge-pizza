489ba75895c88a283dfbe08a7b8d753d4d4d4b97
Adam Mwaniki <adamndegwa@protonmail.com>
2024-07-09 02:12:16 +0300

    Makes get and delete routes for restaurants

--- server/app.py
+++ server/app.py
@@ -42,6 +42,50 @@ class Restaurants(Resource):
 # Remembering to add the api resource
 api.add_resource(Restaurants, '/restaurants')
 
+class RestaurantsByID(Resource):
+    def get(self, id):
+        # making use of a dictionary since they will be inside the list of dictionaries from the Restaurants class
+        restaurant_specific = Restaurant.query.filter_by(id=id).first()
+        
+        if restaurant_specific:
+            # we handle the conversion to dict here instead of in the previous attribute because of the possibility
+            # of a single restaurant that doesn't exist being returned which would result in a 500 error instead of 404
+            # because of the None
+            response_dict = restaurant_specific.to_dict()
+
+            response = make_response(
+                jsonify(response_dict),
+                200
+            )
+        else:
+            response = make_response(
+                jsonify({"error": "Restaurant not found"}),
+                404
+            )
+
+        return response
+    
+    def delete(self, id):
+        restaurant_specific = Restaurant.query.filter(Restaurant.id == id).first()
+
+        db.session.delete(restaurant_specific)
+        db.session.commit()
+
+        response_dict = {"error": "Restaurant not found"}
+
+        response = make_response(
+            response_dict,
+            204
+        )
+
+        return response
+    
+api.add_resource(RestaurantsByID, '/restaurants/<int:id>')
+
+class Pizzas:
+    def get(self):
+        pass
+
 
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
